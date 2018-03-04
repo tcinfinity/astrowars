7e648ad7fec89b9a7f7876d12f8d425a9237b966
tcinfinity <32245869+tcinfinity@users.noreply.github.com>
2018-03-04 19:53:57 +0800

    server.js - node init

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app     = express();
+const http    = require('http');
+const server  = http.createServer(app);
+const path    = require('path');
+const io      = require('socket.io')(server);
+const uuid    = require('uuid/v4');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/index.html"));
+})
+
+io.on('connection', socket => {
+    let clientuuid = uuid();
+    io.emit('newpjoin', {uuid: clientuuid})
+    console.log('user connected: ' + clientuuid);
+});
+
+server.listen(6262, () => {
+    console.log("listening on port 8000");
+})
